fb25fb84c1ee48e0373c92eb026240eaa80b01f1
Andrew Zajac <ajzajac@icloud.com>
2019-05-16 15:48:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
-function addElementTobeginningOfArray(a,b)
+function addElementToBeginningOfArray(a,b){
+  return chocolateBars.unshift(a)
+}
