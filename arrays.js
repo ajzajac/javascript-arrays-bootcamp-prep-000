40b4b1e87abb372cc49f25e0b22839c1c90caf8f
Andrew Zajac <ajzajac@icloud.com>
2019-05-16 17:29:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,6 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
 function addElementToEndOfArray(chocolateBars, candyString){
   return [...chocolateBars, candyString];
 }
+function destructivelyaddElementToEndOfArray(chocolateBars, candyString){
+  return chocolateBars.push(candyString);
+}
