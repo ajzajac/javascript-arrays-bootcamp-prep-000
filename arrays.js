1ff5fd1af8d57ea3aaacd860608bab59ad38362e
Andrew Zajac <ajzajac@icloud.com>
2019-05-16 16:52:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 
 function addElementToBeginningOfArray(array,element){
-   return ['foo',...chocolateBars];
+   return ('foo',...chocolateBars);
 }
